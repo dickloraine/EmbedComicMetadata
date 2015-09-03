3b9eb6bfb1da597f493b65c4639407a3f4974028
dickloraine <dickloraine@gmx.net>
2015-09-03 13:20:11 +0200

    consistent tabs

--- __init__.py
+++ __init__.py
@@ -1,7 +1,7 @@
 #!/usr/bin/env python2
 # vim:fileencoding=UTF-8:ts=4:sw=4:sta:et:sts=4:ai
 from __future__ import (unicode_literals, division, absolute_import,
-                        print_function)
+						print_function)
 
 __license__   = 'GPL v3'
 __copyright__ = '2015, dloraine'
@@ -11,70 +11,68 @@ __docformat__ = 'restructuredtext en'
 from calibre.customize import InterfaceActionBase
 
 class EmbedComicMetadataBase(InterfaceActionBase):
-    '''
-    This class is a simple wrapper that provides information about the actual
-    plugin class. The actual interface plugin class is called InterfacePlugin
-    and is defined in the ui.py file, as specified in the actual_plugin field
-    below.
+	'''
+	This class is a simple wrapper that provides information about the actual
+	plugin class. The actual interface plugin class is called InterfacePlugin
+	and is defined in the ui.py file, as specified in the actual_plugin field
+	below.
 
-    The reason for having two classes is that it allows the command line
-    calibre utilities to run without needing to load the GUI libraries.
-    '''
-    name                = 'Embed Comic Metadata'
-    description         = 'Embeds calibres metadata into cbz comic archieves'
-    supported_platforms = ['windows', 'osx', 'linux']
-    author              = 'dloraine'
-    version             = (1, 0, 1)
-    minimum_calibre_version = (1, 0, 0)
+	The reason for having two classes is that it allows the command line
+	calibre utilities to run without needing to load the GUI libraries.
+	'''
+	name                = 'Embed Comic Metadata'
+	description         = 'Embeds calibres metadata into cbz comic archieves'
+	supported_platforms = ['windows', 'osx', 'linux']
+	author              = 'dloraine'
+	version             = (1, 0, 1)
+	minimum_calibre_version = (1, 0, 0)
 
-    #: This field defines the GUI plugin class that contains all the code
-    #: that actually does something. Its format is module_path:class_name
-    #: The specified class must be defined in the specified module.
-    actual_plugin       = 'calibre_plugins.EmbedComicMetadata.ui:EmbedComicMetadata'
+	#: This field defines the GUI plugin class that contains all the code
+	#: that actually does something. Its format is module_path:class_name
+	#: The specified class must be defined in the specified module.
+	actual_plugin       = 'calibre_plugins.EmbedComicMetadata.ui:EmbedComicMetadata'
 
-    def is_customizable(self):
-        '''
-        This method must return True to enable customization via
-        Preferences->Plugins
-        '''
-        return True
+	def is_customizable(self):
+		'''
+		This method must return True to enable customization via
+		Preferences->Plugins
+		'''
+		return True
 
-    def config_widget(self):
-        '''
-        Implement this method and :meth:`save_settings` in your plugin to
-        use a custom configuration dialog.
+	def config_widget(self):
+		'''
+		Implement this method and :meth:`save_settings` in your plugin to
+		use a custom configuration dialog.
 
-        This method, if implemented, must return a QWidget. The widget can have
-        an optional method validate() that takes no arguments and is called
-        immediately after the user clicks OK. Changes are applied if and only
-        if the method returns True.
+		This method, if implemented, must return a QWidget. The widget can have
+		an optional method validate() that takes no arguments and is called
+		immediately after the user clicks OK. Changes are applied if and only
+		if the method returns True.
 
-        If for some reason you cannot perform the configuration at this time,
-        return a tuple of two strings (message, details), these will be
-        displayed as a warning dialog to the user and the process will be
-        aborted.
+		If for some reason you cannot perform the configuration at this time,
+		return a tuple of two strings (message, details), these will be
+		displayed as a warning dialog to the user and the process will be
+		aborted.
 
-        The base class implementation of this method raises NotImplementedError
-        so by default no user configuration is possible.
-        '''
-        # It is important to put this import statement here rather than at the
-        # top of the module as importing the config class will also cause the
-        # GUI libraries to be loaded, which we do not want when using calibre
-        # from the command line
-        from calibre_plugins.EmbedComicMetadata.config import ConfigWidget
-        return ConfigWidget()
+		The base class implementation of this method raises NotImplementedError
+		so by default no user configuration is possible.
+		'''
+		# It is important to put this import statement here rather than at the
+		# top of the module as importing the config class will also cause the
+		# GUI libraries to be loaded, which we do not want when using calibre
+		# from the command line
+		from calibre_plugins.EmbedComicMetadata.config import ConfigWidget
+		return ConfigWidget()
 
-    def save_settings(self, config_widget):
-        '''
-        Save the settings specified by the user with config_widget.
+	def save_settings(self, config_widget):
+		'''
+		Save the settings specified by the user with config_widget.
 
-        :param config_widget: The widget returned by :meth:`config_widget`.
-        '''
-        config_widget.save_settings()
+		:param config_widget: The widget returned by :meth:`config_widget`.
+		'''
+		config_widget.save_settings()
 
-        # Apply the changes
-        ac = self.actual_plugin_
-        if ac is not None:
-            ac.apply_settings()
-
-
+		# Apply the changes
+		ac = self.actual_plugin_
+		if ac is not None:
+			ac.apply_settings()
--- ui.py
+++ ui.py
@@ -1,16 +1,16 @@
 #!/usr/bin/env python2
 # vim:fileencoding=UTF-8:ts=4:sw=4:sta:et:sts=4:ai
 from __future__ import (unicode_literals, division, absolute_import,
-                        print_function)
+						print_function)
 
 __license__   = 'GPL v3'
 __copyright__ = '2015, dloraine'
 __docformat__ = 'restructuredtext en'
 
 try:
-    from PyQt5.Qt import QToolButton, QMenu
+	from PyQt5.Qt import QToolButton, QMenu
 except ImportError:
-    from PyQt4.Qt import QToolButton, QMenu
+	from PyQt4.Qt import QToolButton, QMenu
 # The class that all interface action plugins must inherit from
 from calibre.gui2.actions import InterfaceAction
 from calibre.gui2 import error_dialog
@@ -20,29 +20,29 @@ from calibre_plugins.EmbedComicMetadata.config import prefs
 
 class EmbedComicMetadata(InterfaceAction):
 
-    name = 'Embed Comic Metadata'
+	name = 'Embed Comic Metadata'
 
-    # Declare the main action associated with this plugin
-    action_spec = ('Embed Comic Metadata', None,
-            'Run the Embed Comic Metadata Plugin', None)
+	# Declare the main action associated with this plugin
+	action_spec = ('Embed Comic Metadata', None,
+			'Run the Embed Comic Metadata Plugin', None)
 
-    def genesis(self):
-        # This method is called once per plugin, do initial setup here
+	def genesis(self):
+		# This method is called once per plugin, do initial setup here
 		self.menu = QMenu(self.gui)
 		self.build_menu()
-				
-        # Set the icon for this interface action
-        icon = get_icons('images/icon.png')
 
-        # The qaction is automatically created from the action_spec defined
-        # above
+		# Set the icon for this interface action
+		icon = get_icons('images/icon.png')
+
+		# The qaction is automatically created from the action_spec defined
+		# above
 		self.qaction.setMenu(self.menu)
-        self.qaction.setIcon(icon)
-        self.qaction.triggered.connect(self.menu_triggered)
+		self.qaction.setIcon(icon)
+		self.qaction.triggered.connect(self.menu_triggered)
 
 	def build_menu(self):
 		m = self.menu
-        m.clear()
+		m.clear()
 		self.create_menu_action(m, "embed", "Embed both Comic Metadata types", icon=None, shortcut=None,
 							description=None, triggered=self.both_triggered, shortcut_name=None)
 		self.create_menu_action(m, "embedcbi", "Only embed Metadata in comment", icon=None, shortcut=None,
@@ -55,7 +55,7 @@ class EmbedComicMetadata(InterfaceAction):
 		self.create_menu_action(m, "configure", "Configure", icon=None, shortcut=None,
 							description=None, triggered=self.configure_triggered, shortcut_name=None)
 		
-    def menu_triggered(self):
+	def menu_triggered(self):
 		# Check the preferences for what should be embedded
 		if prefs['cbi_embed'] and prefs['cix_embed']:
 			do_embed = "both"
@@ -65,9 +65,9 @@ class EmbedComicMetadata(InterfaceAction):
 			do_embed = "cix"
 		else:
 			return error_dialog(self.gui, 'Cannot update metadata',
-						 'No embed format selected', show=True)
+						'No embed format selected', show=True)
 		# embed the metadata
-        update_metadata(self, do_embed)
+		update_metadata(self, do_embed)
 		
 	def both_triggered(self):
 		update_metadata(self, "both")
@@ -84,8 +84,8 @@ class EmbedComicMetadata(InterfaceAction):
 	def configure_triggered(self):
 		self.interface_action_base_plugin.do_user_config(self.gui)
 
-    def apply_settings(self):
-        from calibre_plugins.EmbedComicMetadata.config import prefs
-        # In an actual non trivial plugin, you would probably need to
-        # do something based on the settings in prefs
-        prefs
+	def apply_settings(self):
+		from calibre_plugins.EmbedComicMetadata.config import prefs
+		# In an actual non trivial plugin, you would probably need to
+		# do something based on the settings in prefs
+		prefs
