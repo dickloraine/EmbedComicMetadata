dbd5e3b9a9bfbf620279a21935d4a8e5f7670ad5
dickloraine <dickloraine@gmx.net>
2016-01-23 00:21:37 +0100

    some changes

--- comicbookinfo.py
+++ comicbookinfo.py
@@ -1,5 +1,5 @@
 """
-A python class to encapsulate the ComicBookInfo data 
+A python class to encapsulate the ComicBookInfo data
 """
 
 """
@@ -27,24 +27,24 @@ from calibre.utils.localization import calibre_langcode_to_name, canonicalize_la
 from calibre_plugins.EmbedComicMetadata.genericmetadata import GenericMetadata
 
 class ComicBookInfo:
-		 
+
 
 	def metadataFromString( self, string ):
-	
+
 		cbi_container = json.loads( unicode(string, 'utf-8') )
 
 		metadata = GenericMetadata()
 
 		cbi = cbi_container[ 'ComicBookInfo/1.0' ]
 
-		#helper func 
+		#helper func
 		# If item is not in CBI, return None
 		def xlate( cbi_entry):
 			if cbi_entry in cbi:
-				return cbi[cbi_entry] 
-			else:	
-				return None 
-		
+				return cbi[cbi_entry]
+			else:
+				return None
+
 		metadata.series =            xlate( 'series' )
 		metadata.title =             xlate( 'title' )
 		metadata.issue =             xlate( 'issue' )
@@ -61,51 +61,51 @@ class ComicBookInfo:
 		metadata.country =           xlate( 'country' )
 		metadata.criticalRating =    xlate( 'rating' )
 		metadata.tags =              xlate( 'tags' )
-		
+
 		# make sure credits and tags are at least empty lists and not None
 		if metadata.credits is None:
 			metadata.credits = []
 		if metadata.tags is None:
 			metadata.tags = []
-			
-		#need to massage the language string to be ISO 
+
+		#need to massage the language string to be ISO
 		# modified to use a calibre function
 		if metadata.language is not None:
 			metadata.language = lang_as_iso639_1(metadata.language)
-		
+
 		metadata.isEmpty = False
-		
+
 		return metadata
 
 	def stringFromMetadata( self, metadata ):
 
 		cbi_container = self.createJSONDictionary( metadata )
 		return json.dumps( cbi_container )
-	
+
 	#verify that the string actually contains CBI data in JSON format
 	def validateString( self, string ):
-		
+
 		try:
 			cbi_container = json.loads( string )
 		except:
 			return False
-			
+
 		return ( 'ComicBookInfo/1.0' in cbi_container )
 
 
 	def createJSONDictionary( self, metadata ):
-		
+
 		# Create the dictionary that we will convert to JSON text
 		cbi = dict()
-		cbi_container = {'appID' : 'ComicTagger/', 
-		                 'lastModified' : str(datetime.now()), 
+		cbi_container = {'appID' : 'ComicTagger/',
+		                 'lastModified' : str(datetime.now()),
 		                 'ComicBookInfo/1.0' : cbi }
-		
+
 		#helper func
 		def assign( cbi_entry, md_entry):
 			if md_entry is not None:
 				cbi[cbi_entry] = md_entry
-			
+
 		#helper func
 		def toInt(s):
 			i = None
@@ -115,7 +115,7 @@ class ComicBookInfo:
 				except ValueError:
 					pass
 			return i
-				
+
 		assign( 'series', metadata.series )
 		assign( 'title', metadata.title )
 		assign( 'issue', metadata.issue )
@@ -132,7 +132,5 @@ class ComicBookInfo:
 		assign( 'rating', metadata.criticalRating )
 		assign( 'credits', metadata.credits )
 		assign( 'tags', metadata.tags )
-		
-		return cbi_container
-	
 
+		return cbi_container
